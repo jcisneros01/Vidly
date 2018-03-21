a550e1909cc577e06d0e20152ce10790339da22b
Jose Cisneros <100jcisneros@mawcolo.wish.org>
2018-03-20 17:01:42 -0700

    changed model

---
+++ Vidly/Migrations/201803202232077_PopulateMembershipTypes.cs
@@ -0,0 +1,17 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateMembershipTypes : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) values (1,0,0,0)");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -8,6 +8,13 @@ namespace Vidly.Models
 	public class Customer
 	{
 		public int Id { get; set; }
-		public string Name { get; set; }
-	}
+
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public MembershipType MembershipType { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+    }
 }
